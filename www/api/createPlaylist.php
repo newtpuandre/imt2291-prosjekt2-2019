1f28cc634a6611756ab1c9227f7ea33735c7aab1
newtpuandre <andgun96@gmail.com>
2019-04-14 15:03:22 +0200

    Lagt til skall for createPlaylist php api

---
+++ www/api/createPlaylist.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+$http_origin = $_SERVER['HTTP_ORIGIN'];
+
+if ($http_origin == "http://www" || $http_origin == "http://localhost:8080") {
+    header("Access-Control-Allow-Origin: $http_origin");
+}
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Origin");
+header("Access-Control-Allow-Credentials: true");
+header("Content-Type: application/json; charset=utf-8");
+
+require_once 'classes/DB.php';
+$db = DB::getDBConnection();
+$res = [];
+
+if (isset($_SESSION['uid'])) {
+
+    //Create playlist
+
+    //if successfull
+    //return success
+    //else
+    //return error.
+
+
+}
