8fa2fddda88b5ba60f9f4bfc85aad2d98dc5b501
newtpuandre <andgun96@gmail.com>
2019-04-15 13:51:28 +0200

    Cleanup

--- www/api/getUsers.php
+++ www/api/getUsers.php
@@ -6,12 +6,7 @@ header("Access-Control-Allow-Headers: Origin");
 header("Content-Type: application/json; charset=utf-8");
 
 require_once 'classes/admin.php';
-//$db = DB::getDBConnection();
 $admin = new Admin();
 
-//$stmt = $db->prepare('SELECT id, name, email, privileges, isTeacher FROM users');
-
-//$stmt->execute(array());
 $res = $admin->gatherUsers();
 echo json_encode($res);
-//echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
